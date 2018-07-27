7646343c5990061630e8bb649202782debfcb9fe
Vedmaka <god.vedmaka@gmail.com>
2018-07-27 21:12:14 +0300

    initial commit

---
+++ footer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+
+	</div><!-- .site-content -->
+
+	<footer id="subscribe-nc">
+		<span class="nc-footer-title">Stay Aware</span>
+		<span class="nc-footer-text">Sign up with your email to receive news & updates</span>
+		<form>
+			<input type="email" placeholder="Email Address" />
+			<input type="submit" value="sign-up" />
+		</form>
+	</footer>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php
+				/**
+				 * Fires before the Twenty Fifteen footer text for footer customization.
+				 *
+				 * @since Twenty Fifteen 1.0
+				 */
+				do_action( 'twentyfifteen_credits' );
+			?>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- .site-footer -->
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
